8463bd67d3f57cc4e024134b5d9185f5031ef3f3
Cedric Vincent H <cedric.hije@aeronology.travel>
2026-05-01 22:06:29 +0800

    Generated components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './component/header/header';
+import { Home } from './pages/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Home],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('angular-test');
+  protected readonly title = signal('angular-user');
 }
