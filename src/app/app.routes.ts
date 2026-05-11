179dc5b54c506c2c169fc11580e627cb828e5e56
Cedric Vincent H <cedric.hije@aeronology.travel>
2026-05-11 21:33:23 +0800

    Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => {
+            return import('./pages/home/home').then(m => m.Home)
+        }
+    },
+    {
+        path: 'todos',
+        loadComponent: () => {
+            return import('./pages/todos/todos').then(m => m.Todos)
+        }
+    }
+];
