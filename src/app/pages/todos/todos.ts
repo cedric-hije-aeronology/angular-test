3e8a14556a71dadab757cfd826d5c86d82c2ec90
Cedric Vincent H <cedric.hije@aeronology.travel>
2026-05-13 21:59:26 +0800

    HTTP call

--- src/app/pages/todos/todos.ts
+++ src/app/pages/todos/todos.ts
@@ -1,13 +1,23 @@
 import { Component, inject, signal } from '@angular/core';
 import { Todos as TodosService } from '../../services/todos';
 import { Todo } from '../../model/todo.type';
+import { catchError, of } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './todos.html',
   styleUrl: './todos.scss',
 })
 export class Todos {
   todoService = inject(TodosService)
+  errorMessage = signal('')
+  todoItems$ = this.todoService.getTodosFromApi().pipe(
+                catchError((err) => {
+                  console.log(err);
+                  this.errorMessage.set(err?.error?.message || err?.message || 'Unknown error occured')
+                  return of([]);
+                })
+              )
 }
--- src/app/services/todos.ts
+++ src/app/services/todos.ts
@@ -1,19 +1,27 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Todo } from '../model/todo.type';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Todos {
-  todoItems: Array<Todo> = [{
-    title: 'groceries',
-    id: 0,
-    userId: 1,
-    completed: false
-  },{
-    title: 'car wash',
-    id: 1,
-    userId: 1,
-    completed: false
-  }]
+  http = inject(HttpClient)
+
+  // todoItems: Array<Todo> = [{
+  //   title: 'groceries',
+  //   id: 0,
+  //   userId: 1,
+  //   completed: false
+  // },{
+  //   title: 'car wash',
+  //   id: 1,
+  //   userId: 1,
+  //   completed: false
+  // }]
+
+  getTodosFromApi() {
+    const url = 'https://jsonplaceholder.typicode.com/todos'
+    return this.http.get<Array<Todo>>(url)
+  }
 }
